3c2c355fa80d4fc28ec53a1062a3a59148d3d09a
Damián Muti <hi@damianmuti.com>
2014-09-17 16:11:35 -0300

    Updated .gitignore. Updated Grunt tasks & comments. Fixed _button module. Fixed _icons module. Cleaned Bower legacy dependencies. Updated Grunt dependencies. Cleaned script.js

--- Gruntfile.js
+++ Gruntfile.js
@@ -5,71 +5,87 @@ module.exports = function (grunt) {
   // Load Grunt tasks declared in the package.json file
   require('load-grunt-tasks')(grunt);
 
+
+
+  // Folders for assets, development environment and production environment
+  var 
+    assets_folder     = 'assets',
+    dev_folder        = 'dev',
+    public_folder     = 'prod';
+
+
+  // Server info
+  var
+    server_hostname   = 'localhost',
+    server_port       = 1337;
+
+
+
+
   // Configure Grunt 
   grunt.initConfig({
 
 
     /* ====================================================================================== */
-    /* publicelopment tasks                                                                      */
+    /* Development tasks                                                                      */
     /* ====================================================================================== */
 
 
-    // grunt-watch monitors the projects files
-    // watch: {
-    //   options: {
-    //     livereload: true
-    //   },
-    //   css: {
-    //       files: ['public/css/styles.css']
-    //   },
-    //   js: {
-    //       files: ['public/js/**.*']
-    //   },
-    //   views: {
-    //       files: ['public/*.html']
-    //   },
-    //   images: {
-    //     files: 'assets/images/*.*',
-    //     tasks: ['copy:images']
-    //   },
-    //   icons: {
-    //     files: 'assets/icon-library/*.*',
-    //     tasks: ['webfont']
-    //   }
-    // },
+    // grunt-watch monitors the projects files and execute actions when a file changes
+    watch: {
+      options: {
+        livereload: true
+      },
+      css: {
+          files: [dev_folder + '/css/styles.css']
+      },
+      js: {
+          files: [dev_folder + '/js/**.*']
+      },
+      views: {
+          files: [dev_folder + '/*.html']
+      },
+      images: {
+        files: assets_folder + '/images/*.*',
+        tasks: ['copy:images']
+      },
+      icons: {
+        files: assets_folder + '/icon-library/*.*',
+        tasks: ['webfont']
+      }
+    },
 
 
-    // // Create an icon font from SVG files insode /icons folder
-    // webfont: {
-    //   icons: {
-    //     src: 'assets/icon-library/*.svg',
-    //     dest: 'public/fonts',
-    //     destCss: 'assets/styles/libs/iconfont',
-    //     options: {
-    //       font: 'icon-font',
-    //       hashes: false,
-    //       engine: 'node',
-    //       stylesheet: 'scss',
-    //       relativeFontPath: '../fonts/',
-    //       // syntax: 'bootstrap',
-    //       htmlDemo: false,
-    //       skip: false, // Set this variable to false to create the icon font. If /icons folder is empty, leave this variable as is
-    //       templateOptions: {
-    //         baseClass: 'ms-icon',
-    //         classPrefix: 'icon-'
-    //       }
-    //     }    
-    //   }
-    // },
+    // Create an icon font from SVG files insode /icons folder
+    webfont: {
+      icons: {
+        src: assets_folder + '/icon-library/*.svg',
+        dest: dev_folder + '/fonts',
+        destCss: assets_folder + '/styles/libs/iconfont',
+        options: {
+          font: 'icon-font',
+          hashes: false,
+          engine: 'node',
+          stylesheet: 'scss',
+          relativeFontPath: '../fonts/',
+          // syntax: 'bootstrap',
+          htmlDemo: false,
+          skip: true, // Set this variable to false to create the icon font. If /icons folder is empty, leave this variable as is
+          templateOptions: {
+            baseClass: 'ms-icon',
+            classPrefix: 'icon-'
+          }
+        }    
+      }
+    },
 
 
-    // // grunt-open will open your browser at the project's URL
-    // open: {
-    //   public: {
-    //     // Gets the port from the connect configuration
-    //     path: 'http://localhost:3000'
-    //   }
-    // },
+    // grunt-open will open your browser at the project's URL
+    open: {
+      source: {
+        path: 'http://localhost:1337'
+      }
+    },
 
 
     // Copy only the needed resources from Bower
@@ -78,151 +94,148 @@ module.exports = function (grunt) {
         // Bower components folder will be removed afterwards
         clean: true
       },
+      // CSS Normalizer
       normalize: {
-        // Target-specific file lists and/or options go here
         options: {
-            destPrefix: 'assets/styles/libs'
+            destPrefix: assets_folder + '/styles/libs'
         },
         files: {
           'normalize': 'normalize.scss'
         }
       },
+      // Jeet Grid System
       jeet: {
-        // Target-specific file lists and/or options go here
         options: {
-            destPrefix: 'assets/styles/libs'
+            destPrefix: assets_folder + '/styles/libs'
         },
         files: {
           'jeet': 'jeet.gs/scss/jeet'
         }
       },
+      // Modernizr
       modernizr: {
-        // Target-specific file lists and/or options go here
         options: {
-            destPrefix: 'public/js/vendor'
+            destPrefix: dev_folder + '/js/vendor'
         },
         files: {
           'modernizr.js': 'modernizr/modernizr.js'
         }
       },
+      // jQuery version for modern browsers
       jquerymodern: {
-        // Target-specific file lists and/or options go here
         options: {
-            destPrefix: 'public/js/vendor'
+            destPrefix: dev_folder + '/js/vendor'
         },
         files: {
           'jquery-2.1.1.js': 'jquery-modern/dist/jquery.js'
         }
       },
 
-      /* Keep legacy support - IE8 */
-      /*
-      jquerylegacy: {
-        // Target-specific file lists and/or options go here
-        options: {
-            destPrefix: 'public/js/vendor'
-        },
-        files: {
-          'jquery-1.11.0.js': 'jquery-legacy/dist/jquery.js'
-        }
-      },
-      jquerymigrate: {
-        // Target-specific file lists and/or options go here
-        options: {
-            destPrefix: 'public/js/vendor'
-        },
-        files: {
-          'jquery-migrate.js': 'jquery-migrate/jquery-migrate.js'
-        }
-      },
-      css3pie: {
-        // Target-specific file lists and/or options go here
-        options: {
-            destPrefix: 'public/scripts'
-        },
-        files: {
-          'PIE.htc': 'css3pie/PIE.htc'
-        }
-      },
-      selectivizr: {
-        // Target-specific file lists and/or options go here
-        options: {
-            destPrefix: 'public/js/vendor'
-        },
-        files: {
-          'selectivizr-1.0.2.js': 'selectivizr/selectivizr.js'
-        }
-      },
-      polyfillboxsizing: {
-        // Target-specific file lists and/or options go here
-        options: {
-            destPrefix: 'public/scripts'
-        },
-        files: {
-          'boxsizing.htc': 'box-sizing-polyfill/boxsizing.htc'
-        }
-      },
-      polyfillplaceholders: {
-        // Target-specific file lists and/or options go here
-        files: {
-          'public/js/plugins/placeholder_polyfill.jquery.min.combo.js': 'html5-placeholder-polyfill/dist/placeholder_polyfill.jquery.min.combo.js',
-          'assets/styles/libs/html5-placeholder-polyfill/_html5-placeholder-polyfill.scss': 'html5-placeholder-polyfill/src/placeholder_polyfill.css'
-        }
-      },
-      */
+
+      
+      // // jQuery version for legacy browser
+      // jquerylegacy: {
+      //   options: {
+      //       destPrefix: 'source/js/vendor'
+      //   },
+      //   files: {
+      //     'jquery-1.11.0.js': 'jquery-legacy/dist/jquery.js'
+      //   }
+      // },
+      // // jQuery version for legacy browsers
+      // jquerymigrate: {
+      //   options: {
+      //       destPrefix: 'source/js/vendor'
+      //   },
+      //   files: {
+      //     'jquery-migrate.js': 'jquery-migrate/jquery-migrate.js'
+      //   }
+      // },
+      // // CSS3 Pie
+      // css3pie: {
+      //   options: {
+      //       destPrefix: 'source/scripts'
+      //   },
+      //   files: {
+      //     'PIE.htc': 'css3pie/PIE.htc'
+      //   }
+      // },
+      // // Selectivizr
+      // selectivizr: {
+      //   options: {
+      //       destPrefix: 'source/js/vendor'
+      //   },
+      //   files: {
+      //     'selectivizr-1.0.2.js': 'selectivizr/selectivizr.js'
+      //   }
+      // },
+      // // Polyfill to support box-sizing
+      // polyfillboxsizing: {
+      //   options: {
+      //       destPrefix: 'source/scripts'
+      //   },
+      //   files: {
+      //     'boxsizing.htc': 'box-sizing-polyfill/boxsizing.htc'
+      //   }
+      // },
+      // // Polyfill to support input placeholders 
+      // polyfillplaceholders: {
+      //   files: {
+      //     'source/js/plugins/placeholder_polyfill.jquery.min.combo.js': 'html5-placeholder-polyfill/dist/placeholder_polyfill.jquery.min.combo.js',
+      //     'assets/styles/libs/html5-placeholder-polyfill/_html5-placeholder-polyfill.scss': 'html5-placeholder-polyfill/src/placeholder_polyfill.css'
+      //   }
+      // },
     },
 
 
-    // // Copy folder to make a publicelopment version of the website
-    // copy: {
-    //   images: {
-    //     expand: true,
-    //     cwd: 'assets/images',
-    //     src: '**',
-    //     dest: 'public/img',
-    //     filter: 'isFile',
-    //   }
-    // },
+    // Every time an image gets updated or a new image is saved in the images folder, Grunt will copy all the images to the source folder
+    copy: {
+      dev: {
+        expand: true,
+        cwd: assets_folder + '/images',
+        src: '**',
+        dest: dev_folder + '/img',
+        filter: 'isFile',
+      }
+    },
 
 
-    // connect: {
-    //   server: {
-    //     options: {
-    //       port: 3000,
-    //       base: 'public/',
-    //       hostname: 'localhost',
-    //       livereload: true
-    //     }
-    //   }
-    // },
+    // Get a local server running
+    connect: {
+      server: {
+        options: {
+          port: server_port,
+          base: dev_folder + '/',
+          hostname: server_hostname,
+          livereload: true
+        }
+      }
+    },
 
 
-    // // Execute concurrent tasks in Grunt
-    // concurrent: {
-    //   watch: {
-    //     tasks: [
-    //       'watch',
-    //       'shell:sass', // Run console command to compile Sass
-    //       //'shell:apache', // Run console command to serve files
-
-    //       'open'
-    //     ],
-    //     options: {
-    //         logConcurrentOutput: true,
-    //         limit: 4
-    //     }
-    //   }
-    // },
+    // Execute concurrent tasks in Grunt
+    concurrent: {
+      watch: {
+        tasks: [
+          'watch',          // Watch if files change
+          'shell:sass',     // Run console command to compile Sass
+          'open'            // Open the server URL in a browser
+        ],
+        options: {
+          logConcurrentOutput: true, 
+          limit: 4 // Limit the cores usage to 4
+        }
+      }
+    },
 
 
     // Run shell commands as a Grunt task
-    // 
-    // shell: {
-    //   // Run Sass compiling with watch, compass and sourcemap flags
-    //   sass: {
-    //     command: 'sass --watch --compass --sourcemap assets/styles/styles.scss:public/css/styles.css'
-    //   }
-    // },
+    shell: {
+      // Run Sass compiling with watch, compass and sourcemap flags
+      sass: {
+        command: 'sass --watch --compass --sourcemap ' + assets_folder + '/styles/styles.scss:' + dev_folder + '/css/styles.css'
+      }
+    },
 
 
 
@@ -231,14 +244,14 @@ module.exports = function (grunt) {
     // /* Production tasks                                                                       */
     // /* ====================================================================================== */
 
-    // // Compress images
+    // Compress images
     // imagemin: {
     //   dist: {
     //     files: [{
     //       expand: true,
-    //       cwd: 'publicelopment/images',
+    //       cwd: 'public/images',
     //       src: '{,*/}*.{gif,jpeg,jpg,png}',
-    //       dest: 'public/images'
+    //       dest: 'source/images'
     //     }]
     //   }
     // },
@@ -259,11 +272,11 @@ module.exports = function (grunt) {
     //   },
     //   dist: { 
     //     files: [{ 
-    //         expand: true,       // Enable dynamic expansion.
-    //         cwd: 'public/images',     // Src matches are relative to this path.
-    //         src: ['**/*.svg'],  // Actual pattern(s) to match.
-    //         dest: 'public/images',       // Destination path prefix.
-    //         ext: '.svg'     // Dest filepaths will have this extension.
+    //         expand: true,             // Enable dynamic expansion.
+    //         cwd: 'source/images',     // Src matches are relative to this path.
+    //         src: ['**/*.svg'],        // Actual pattern(s) to match.
+    //         dest: 'public/images',    // Destination path prefix.
+    //         ext: '.svg'               // Dest filepaths will have this extension.
     //     }]
     //   }
     // },
@@ -273,14 +286,14 @@ module.exports = function (grunt) {
     // uglify: {
     //   dist: {
     //     files: {
-    //       'public/scripts/main.js': [
-    //           'public/scripts/main.js'
+    //       'source/scripts/main.js': [
+    //           'source/scripts/main.js'
     //       ],
-    //       'public/scripts/vendor.js': [
-    //           'public/scripts/vendor.js'
+    //       'source/scripts/vendor.js': [
+    //           'source/scripts/vendor.js'
     //       ],
-    //       'public/scripts/plugins.js': [
-    //           'public/scripts/plugins.js'
+    //       'source/scripts/plugins.js': [
+    //           'source/scripts/plugins.js'
     //       ]
     //     }
     //   }
@@ -290,39 +303,27 @@ module.exports = function (grunt) {
     // // Concatenate JS files
     // concat: {
     //   vendor: {
-    //     src: [ 'public/scripts/vendor/*.js' ],
-    //     dest: 'public/scripts/vendor.js'
+    //     src: [ 'source/scripts/vendor/*.js' ],
+    //     dest: 'source/scripts/vendor.js'
     //   },
     //   plugins: {
-    //     src: [ 'public/scripts/plugins/*.js' ],
-    //     dest: 'public/scripts/plugins.js'
+    //     src: [ 'source/scripts/plugins/*.js' ],
+    //     dest: 'source/scripts/plugins.js'
     //   }
     // },
 
 
-    // // Clean up unnecessary files while building public version
+    // // Clean up unnecessary files while building source version
     // clean: {
     //   build: {
     //     src: [
-    //       "public/css/*.map", 
-    //       "public/scripts/vendor", 
-    //       "public/scripts/plugins", 
-    //       "public/images/*.md",  
-    //       "public/css/*.md", 
-    //       "public/fonts/*.md"
+    //       "source/css/*.map", 
+    //       "source/scripts/vendor", 
+    //       "source/scripts/plugins", 
+    //       "source/images/*.md",  
+    //       "source/css/*.md", 
+    //       "source/fonts/*.md"
     //     ]
-    //   }
-    // },
-
-
-    // // Compass tasks for Grunt
-    // compass: {  
-    //   dist: { 
-    //     options: {  
-    //       sassDir: 'sass',
-    //       cssDir: 'publicelopment/css',
-    //       environment: 'production'
-    //     }
     //   }
     // },
 
@@ -331,57 +332,22 @@ module.exports = function (grunt) {
     // copy: {
     //   main: {
     //     expand: true,
-    //     cwd: 'publicelopment/',
+    //     cwd: 'public/',
     //     src: '**',
-    //     dest: 'public/',
+    //     dest: 'source/',
     //     filter: 'isFile',
     //   },
     // },
 
 
-    // // Minify CSS for public/production release
+    // // Minify CSS for source/production release
     // cssmin: {
     //   minify: {
     //     expand: true,
-    //     cwd: 'public/css/',
+    //     cwd: 'source/css/',
     //     src: ['*.css'],
-    //     dest: 'public/css/',
+    //     dest: 'source/css/',
     //     ext: '.css'
-    //   }
-    // }
-
-    
-
-    // kss: {
-    //   options: {
-    //     includeType: 'css',
-    //     includePath: './prod/css',
-    //     template: './template2'
-    //   },
-    //   dist: {
-    //       files: {
-    //         'styleguide': ['./public/sass/styles.scss']
-    //       }
-    //   }
-    // },
-
-
-    // styleguide: {
-    //   options: {
-    //     template: {
-    //       src: 'template2',
-    //       include: ['public/sass/**/*.scss']
-    //     },
-    //     framework: {
-    //       name: 'kss'
-    //     }
-    //   },
-    //   all: {
-    //     files: [{
-    //       'styleguide': 'public/sass/styles.scss',
-    //       'styleguide/helpers': 'public/sass/helpers/_helpers.scss',
-    //       'styleguide/': 'prod/css/styles.css'
-    //     }]
     //   }
     // }
   });
@@ -394,19 +360,11 @@ module.exports = function (grunt) {
     grunt.loadNpmTasks('grunt-contrib-copy');
     grunt.loadNpmTasks('grunt-contrib-connect');
 
-    // grunt.loadNpmTasks('grunt-styleguide');
-    // grunt.loadNpmTasks('grunt-kss');
-
 
 
   /* ====================================================================================== */
   /* Tasks @registration                                                                    */
   /* ====================================================================================== */
-
-  // grunt.registerTask('styleguide', [
-  //   'shell',
-  //   'styleguide'
-  // ]);
 
   // grunt.registerTask('build', [
   //   'bowercopy',
@@ -416,10 +374,10 @@ module.exports = function (grunt) {
 
   grunt.registerTask('run', [
     'bowercopy',
-    // 'copy',
-    // 'connect:server',
-    // 'webfont',
-    // 'concurrent:watch'
+    'copy',
+    'connect:server',
+    'webfont',
+    'concurrent:watch'
   ]);
 
   // // Creates the 'build' task
